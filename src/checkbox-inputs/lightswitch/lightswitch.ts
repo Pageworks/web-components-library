1f4da68c240b8cc62f3e4adf898c579c59a23011
Kyle Andrews <kylea@page.works>
2019-09-23 14:16:57 -0400

    Adds checkbox input component

--- src/checkbox-inputs/lightswitch/lightswitch.ts
+++
@@ -1,9 +0,0 @@
-class LightswitchComponent extends HTMLElement
-{
-	connectedCallback()
-	{
-		console.log('lightswitch component has been connected.');
-	}
-}
-
-customElements.define('lightswitch-component', LightswitchComponent);
